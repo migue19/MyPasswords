54ca90d80f0f267ea26f3b33571bc2ccc3113a0a
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2024-01-22 11:05:29 -0600

    fetch data

---
+++ MyPasswords/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MyPasswords
+//
+//  Created by Miguel Mexicano Herrera on 22/01/24.
+//
+
+import Foundation
+struct Constants {
+    static let dbName = "MyPasswords"
+    static let name = "name"
+    static let user = "user"
+    static let password = "password"
+}
--- MyPasswords/Manager/CoreDataManager.swift
+++ MyPasswords/Manager/CoreDataManager.swift
@@ -8,11 +8,29 @@
 import UIKit
 import CoreData
 class CoreDataManager {
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var context: NSManagedObjectContext!
     private static var shared: CoreDataManager = {
         let manager = CoreDataManager()
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        manager.context = appDelegate.persistentContainer.viewContext
         return manager
     }()
-
+    func fetchData() -> [ListPasswordEntity] {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: Constants.dbName)
+        request.returnsObjectsAsFaults = false
+        var passwords: [ListPasswordEntity] = []
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                let name: String = data.value(forKey: Constants.name) as! String
+                let user: String = data.value(forKey: Constants.user) as! String
+                let password: String = data.value(forKey: Constants.password) as! String
+                passwords.append(ListPasswordEntity(name: name, user: user, password: password))
+            }
+            return passwords
+        } catch {
+            print("Error al obtener los datos")
+            return []
+        }
+    }
 }
